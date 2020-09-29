258687a07ae958d0d23ad9e0c78f0ea96e13c266
Deep Patel <deeppatel@live.com>
2020-09-29 02:53:09 -0400

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,4 @@
 //READ JSON USING D3
-
 d3.json("samples.json").then(function(sampleData){
     console.log(sampleData);
 
@@ -9,19 +8,31 @@ d3.json("samples.json").then(function(sampleData){
     var otu_labels = sampleData.samples[0].otu_labels
     
     //Slice to get the Top 10s of ID, Values. and their respective Labels
+        
         //IDs
-        var OTU_Top_ID_Values = otu_ids.slice(0,10);
+        var OTU_Top_ID_Values = otu_ids.slice(0,10).reverse();
             //Set ID's to desired format for graph (Adding the "OTU" before ID number.. ie. OTU 1167)
             var OTU_Top_ID = OTU_Top_ID_Values.map(d => "OTU " + d) 
-        console.log(OTU_Top_ID);
+        //console.log(OTU_Top_ID);
 
         //Values
-        var OTU_Top_Values = otu_values.slice(0,10);
-        console.log(OTU_Top_Values);
+        var OTU_Top_Values = otu_values.slice(0,10).reverse();
+        //console.log(OTU_Top_Values);
 
         //Labels for Hoverover
-        var OTU_Top_Labels = otu_labels.slice(0,10);
-        console.log(OTU_Top_Labels);
+        var OTU_Top_Labels = otu_labels.slice(0,10).reverse();
+        //console.log(OTU_Top_Labels);
 
-    
+    //BAR CHART
+    var trace = {
+        x: OTU_Top_Values,
+        y: OTU_Top_ID,
+        mode: "markers",
+        text: OTU_Top_Labels,
+        type: "bar",
+        orientation: "h",
+    };
+    var data = [trace];
+    var layout = {title: "Top 10 OTUs"};
+    Plotly.newPlot("bar", data, layout);
 }); 
